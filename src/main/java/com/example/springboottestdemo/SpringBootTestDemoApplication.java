9297b86c613da501fee3cc7bf41793acc4a8d0c7
rino <rino@yahoo-corp.jp>
2017-07-18 01:18:15 +0900

    setup using Spring Initializr

---
+++ src/main/java/com/example/springboottestdemo/SpringBootTestDemoApplication.java
@@ -0,0 +1,12 @@
+package com.example.springboottestdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTestDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootTestDemoApplication.class, args);
+	}
+}
