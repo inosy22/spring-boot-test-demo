83b4f9b3623860438c7266569486067910c8c70a
rino <rino@yahoo-corp.jp>
2017-07-20 21:44:43 +0900

    delete answer from master branch

--- src/test/java/com/example/springboottestdemo/UserMapperTests.java
+++ src/test/java/com/example/springboottestdemo/UserMapperTests.java
@@ -1,30 +1,5 @@
 package com.example.springboottestdemo;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
-import java.util.List;
-
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.jdbc.Sql;
-import org.springframework.test.context.junit4.SpringRunner;
-
-@RunWith(SpringRunner.class)
-@SpringBootTest
-@ActiveProfiles("test")
-@Sql("classpath:/delete-test-data.sql") // テーブルのデータを事前削除するスクリプト
 public class UserMapperTests {
-    @Autowired
-    UserMapper userMapper;
     
-    @Test
-    public void insertUsersCountTest() throws Exception {
-        userMapper.insert("hoge", 1);
-        userMapper.insert("fuga", 2);
-        List<User> users = userMapper.findAll();
-        assertThat(users.size()).isEqualTo(2);
-    }
 }
